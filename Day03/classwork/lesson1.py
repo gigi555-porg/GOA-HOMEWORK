dcb9437dd8290ced5704db98b9b7d9b72bd8d3f5
gigi555-porg <futkaradzegigi4@gmail.com>
2024-12-27 00:23:18 +0400

    commit

--- Day03/classwork/lesson1.py
+++ Day03/classwork/lesson1.py
@@ -1,17 +1,1 @@
-#snake case 
-
-#ცვლადის
-
- 
-
-
-
-
-number1 = "gigi" 
-number2 = "futkaradze"
-number3 = 19 
-
-
-number = 5
-number = 5
-print(number1, number2, number3)
+print("no class work")
