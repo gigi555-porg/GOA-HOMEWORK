384e343315f5d75e49b4bc44810cc043be7bf2ab
gigi555-porg <futkaradzegigi4@gmail.com>
2024-12-27 19:35:50 +0400

    commit

---
+++ Day04/classwork/lesson1.py
@@ -0,0 +1,22 @@
+user_name = "gigi"
+user_surname = "futkaradze"
+print(user_name + " " + user_surname)
+#ეს არის string 
+
+
+
+
+num1 = 10 
+num2 = 5 
+#ეს არის integer 
+
+
+print(10 + 5) 
+print(10 - 5)
+print(10 * 5) 
+print(10 / 5) 
+#ეს არის float 
+
+
+
+#კონკატინაცია - concatination - სტრინგების შეერთება 
---
+++ Day04/homework/homework.py
@@ -0,0 +1,53 @@
+#1) დაადეკლარირეთ(შექმენით) 3 ინტეჯერ ტიპის ცვლადი სადაც შეინახავთ მთელ რიცხვებს და დაბეჭდავთ(print) მათ ჯამს
+
+num1 = 30 #int
+num2 = 10 #int
+num3 = 5  #int
+
+print(num1 + num2 + num3) 
+print (num1 - num2 - num3) 
+print (num1 * num2 * num3) 
+print(num1 / num2 / num3)  
+
+
+
+
+# 2) დაადეკლარირეთ სტრინგ ტიპის ცვლადი სახელად name რომელსაც მიანიჭებთ მნიშვნელობად თქვენს სახელს 
+
+
+name = "gigi" #str
+surname = "futkaradze"  #str 
+
+
+print(name + surname) 
+print("gigi" + " " + "futkaradze") #კონკატინაცია
+
+
+
+#3) დაადეკლარირეთ float ტიპის ცვლადი სახელად height რომელსაც მიანიჭებთ მნიშვმელობად თქვენს სიმაღლეს 
+
+
+height = 174.0 #float
+
+print("height 174.0")  
+
+
+
+#4) დაადეკლარირეთ ინტეჯერ ტიპის ცვლადი სახელად x რომელსაც თავდაპირველი მნიშვნელობა იქნება 15 შემდეგ მიუმატეთ 5 და ისე დაბეჭდეთ ცვლადი
+
+
+
+name = "gigi" #str
+surname = "futkaradze" #str 
+
+print("name + surname")
+print("gigi + furtkaradze") 
+ 
+
+x = 15 #int 
+ 
+price = 100 * 5 
+print("price")
+ 
+price2 = 100 
+print(price * price2 str(5)) 
