2dbb0a264c9acbbb9988e3c8f5351372dc158130
gigi555-porg <futkaradzegigi4@gmail.com>
2024-12-31 00:44:54 +0400

    commit

---
+++ Day10/classwork/lesson2.py
@@ -0,0 +1,10 @@
+#2) მომხამრებელს შემოატანინეთ ახლანდელი ასაკი ასევე შემოატანინეთ ახლანდელი წელი, შემდეგ შეეკითხეთ რამდენი წლით სურს დროში მოგზაურობა შემდეგ კი დაბეჭდეთ ერთ გრძელ წინადადებაში რამდენი წლის იქნება მომხარებელი დროში მოგზაურობის შემდეგ და რომელი წელიწადი იქნება დროში მოგზაურობის შემდეგ
+
+
+#age = input("Your Age")  
+
+currnet_age = int(input("Enter Your Age "))
+currnet_year = int(input("Enter currnet Year: "))
+time_travel = int(input("how many years do you want to travel in time: ")) 
+
+print(" when you travel in time your age is will be: " + str(currnet_age + time_travel) + " After time travel you will be there " + str(currnet_year + time_travel) + " in year") 
