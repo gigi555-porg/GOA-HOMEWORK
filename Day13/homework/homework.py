e152db8d75310a744917ffd08abfaf63a15c4e4a
gigi555-porg <futkaradzegigi4@gmail.com>
2025-01-03 20:21:35 +0400

    commit

---
+++ Day13/homework/homework.py
@@ -0,0 +1,63 @@
+# print(მომხმარებელს შემოატაინეთ ორი რიცხვი და მათზე მოახდინეთ მათემატიკური ოპერაციები.)
+
+
+# num1 = int(input("enter your number")) 
+# num2 = int(input("enter your number"))
+
+# print( num1 + num2) 
+# print( num1 - num2)
+# print( num1 * num2)
+# print( num1 / num2) 
+
+ 
+#2)მოიყვანეთ 10 მაგალითი შედარების ოპერატორებზე ==,!=,>,<,=<,=>
+
+
+
+# print( 10 == 10)
+# print( 15 != 27)
+# print( 5 <= 20)
+# print( 15 >= 30)
+# print( 30 < 15)
+# print( 10 == 10)
+# print( 10 > 5 )
+ 
+
+#3) მომხმარებელს შემოატაინეთ თავისი სახელი, გვარი, ასაკი, და შემდეგ შეაერთეთ ისე რომ გამოვიდეს წინადადება. 
+
+
+# name = input("Enter Your Name ")
+# surname = input("Enter Your Surname")
+# age = input("Enter Your Age")
+
+# print("name" + " " + "surname" + " " + "age" + " ")  
+
+#print("saxeli " + " " + name + " " + "gvari" + " " + age + " " + "asaki") 
+
+
+
+#5) მომხმარებელს შემოატაინეთ სახელი, გვარი,ასაკი, საცხოვრებელი ადგილი .
+
+
+
+# name = input("enter Your Name")
+# surname =input("Enter Your Surname")
+# age = input("Enter Your Age")
+# placeofresidence = input(">>>>Enter Your placeofresidence")  
+
+
+
+
+#7) მომხმარებელს შემოატაინეთ 2 რიცხვი, და შემდეგ გამოიყენეთ შედარების ოპერატორები რა რიცხვსაც შეიყვანს მომხმარებელი, მასე გამოიყენთ შედარების ოპერატორები ==,!=,<,>,<=,=>.
+
+# num1 = int(input("Enter Number"))
+# num2 = int(input("Enter Number"))
+
+
+# print(num1 == num2) 
+# print(num1 - num2) 
+# print(num1 != num2)
+# print(num1 > num2) 
+# print(num1 < num2) 
+# print(num1 >= num2) 
+# print(num1 <= num2)
---
+++ Day14/classwork/lesson1.py
@@ -0,0 +1,11 @@
+
+
+# user_age = int(input("Enter Your Age:"))
+
+# print(user_age > 17 and user_age < 28 ) 
+
+
+
+# user_name = input("Enter Your Name: ")  
+
+# print(user_name == "loma" or user_name == "joma")
